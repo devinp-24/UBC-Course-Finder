0fb246988fc50c43c44baeb4c3e123cf22236e1c
AryaanHabib <habibaryaan@gmail.com>
2024-02-13 14:39:14 -0800

    Finished implementation of addDataset

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -23,8 +23,56 @@ export default class InsightFacade implements IInsightFacade {
 	}
 
 	public async addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		return Promise.reject("Not implemented.");
+		let zipFile = JSZip();
+		let info = Array<Promise<string>>();
+		let functionPromise: Promise<string[]>;
+		let dataset: CourseData;
+		return new Promise<string[]>((resolve, reject) => {
+			try {
+				if (!id || id.includes("_") || id.trim() === "") {
+					throw new InsightError("Invalid ID");
+				}
+				if (this.datasetCollection.includes(id)) {
+					throw new InsightError("ID already exists");
+				}
+				if (kind !== InsightDatasetKind.Sections) {
+					throw new InsightError("Invalid Kind Type");
+				}
+				if (content === null) {
+					throw new InsightError("Content should not be empty");
+				}
+				zipFile.loadAsync(content, {base64: true})
+					.then((zip: JSZip) => {
+						if (Object.keys(zip.files)[0] !== "courses/") {
+							return reject(new InsightError("Folder does not exist in the zip file"));
+						}
+						const coursesFolder = zip.folder("courses");
+						if (!coursesFolder) {
+							throw new InsightError("Folder does not exist in the zip file");
+						}
+						coursesFolder.forEach(function (relativePath, file) {
+							info.push(file.async("text"));
+						});
+						Promise.all(info).then((promise: string[]) => {
+							dataset = new CourseData(id, kind, promise);
+							if (dataset.sections.length === 0) {
+								return reject(new InsightError("No valid sections to add"));
+							}
+							this.datasetCollection.push(id);
+							this.courseDataCollection.push(dataset);
+							resolve(this.datasetCollection);
+						}).catch((err: any) => {
+							reject("Error");
+						});
+					}).catch((err: any) => {
+						reject(new InsightError("Failed to add dataset"));
+					});
+			} catch (error) {
+				reject(error);
+			}
+		});
 	}
+
 
 	public async removeDataset(id: string): Promise<string> {
 		return Promise.reject("Not implemented.");
